68665247af938e7f7c17b5abd2f4e37140aa49ee
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2024-04-22 17:44:48 +0700

    change page login

---
+++ public/frontend/js/pwa.js
@@ -0,0 +1,10 @@
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', function () {
+//     navigator.serviceWorker.register('service-worker.js')
+//       .then(registration => {
+//       })
+//       .catch(err => {
+//         console.error('Registration failed:', err);
+//       });
+//   });
+// }
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,95 +1,114 @@
-@extends('layouts.auth')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('title', __('Log in'))
+<head>
+    <meta charset=utf-8>
+    <title>Halaman Login</title>
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" name=viewport>
+    <meta content="" name=description>
+    <meta content="" name=author>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="{{ asset('frontend/auth/css/vendor.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('frontend/auth/css/app.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"
+        integrity="" crossorigin=anonymous referrerpolicy="no-referrer">
 
-@push('css')
-    <link rel="stylesheet" href="{{ asset('mazer') }}/css/pages/auth.css">
-@endpush
 
-@section('content')
-    <div class="row h-100">
-        <div class="col-lg-6 col-12">
-            <div id="auth-left">
-                <div class="auth-logo">
-                    <a href="/">
-                        <img src="{{ asset('mazer') }}/images/logo/logo.svg" alt="Logo">
-                    </a>
+    <style>
+        .page_speed_807555265 {
+            background-image: url({{ asset('frontend/auth/img/login-bg-13.jpg') }})
+        }
+    </style>
+</head>
+
+<body class='pace-top'>
+    <div id="app" class="app">
+        <div class="login login-v2 fw-bold">
+            <div class="login-cover">
+                <div data-id="login-cover-image"class="login-cover-img page_speed_807555265"></div>
+                <div class="login-cover-bg"></div>
+            </div>
+            <div class="login-container small-width">
+                <div class="login-header">
+                    <div class="brand">
+                        <div class="d-flex align-items-center">Dashboard RS</b>
+                        </div>
+                        <small>Please login to start session</small>
+                    </div>
+                    <div class="icon">
+                        <i class="fa fa-lock"></i>
+                    </div>
                 </div>
+                <div class="login-content">
+                    <form action="{{ route('login') }}" method="POST">
+                        @csrf
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="form-floating mb-20px">
+                                    <input type=email name="email"
+                                        class="form-control fs-13px h-45px border-0 @error('email') is-invalid @enderror"
+                                        placeholder="Email" id="email" required value="{{ old('email') }}">
+                                    @error('email')
+                                        <small class="text-danger mt-2">{{ $message }}</small>
+                                    @enderror
+                                    <label for="emailAddress"
+                                        class="d-flex align-items-center text-gray-600 fs-13px">Email Address</label>
+                                </div>
 
-                <h1 class="auth-title">{{ __('Log in.') }}</h1>
-                <p class="auth-subtitle mb-3">{{ __('Log in with your data that you entered during registration.') }}</p>
+                                <div class="form-floating mb-20px">
+                                    <input type=password name="password"
+                                        class="form-control fs-13px h-45px border-0 @error('password') is-invalid @enderror"
+                                        placeholder="Password" id="password" required autocomplete="new-password">
+                                    <label for="emailAddress"
+                                        class="d-flex align-items-center text-gray-600 fs-13px">Password</label>
+                                </div>
 
-                @if ($errors->any())
-                    <div class="alert alert-danger alert-dismissible show fade">
-                        <ul class="ms-0 mb-0">
-                            @foreach ($errors->all() as $error)
-                                <li>
-                                    <p>{{ $error }}</p>
-                                </li>
-                            @endforeach
-                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                        </ul>
-                    </div>
-                @endif
+                                <div class="form-check mb-20px">
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <input class="form-check-input border-0" type="checkbox" value="1"
+                                                id="rememberMe" onclick="myFunction()" />
+                                            <label class="form-check-label fs-13px text-gray-300" for="rememberMe">
+                                                Show Password
+                                            </label>
 
-                @if (session('status'))
-                    <div class="alert alert-success alert-dismissible show fade">
-                        {{ session('status') }}
-                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                    </div>
-                @endif
+                                        </div>
+                                        {{-- <div class="col-6">
+                                            <div class="d-flex justify-content-end">
+                                                <a class="text-right text-decoration-none"
+                                                    href="{{ url('/forget-web') }}">Forgot Password ?</a>
+                                            </div>
+                                        </div> --}}
+                                    </div>
 
-                <form method="POST" action="{{ route('login') }}">
-                    @csrf
 
-                    <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="text" class="form-control form-control-xl @error('email') is-invalid @enderror"
-                            name="email" autocomplete="email" placeholder="Email" required autofocus>
-                        <div class="form-control-icon">
-                            <i class="bi bi-person"></i>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-
-                    <div class="form-group position-relative has-icon-left mb-4">
-                        <input type="password" class="form-control form-control-xl @error('password') is-invalid @enderror"
-                            placeholder="Password" name="password" autocomplete="current-password" required>
-                        <div class="form-control-icon">
-                            <i class="bi bi-shield-lock"></i>
+                        <div class="mb-20px">
+                            <button type=submit class="btn btn-cyan d-block w-100 h-45px btn-lg">Login</button>
                         </div>
-                    </div>
-
-                    <div class="form-check form-check-lg d-flex align-items-end">
-                        <input class="form-check-input me-2" type="checkbox" value="" name="remember" id="remember"
-                            {{ old('remember') ? 'checked' : '' }}>
-                        <label class="form-check-label text-gray-600" for="remember">
-                            Keep me logged in
-                        </label>
-                    </div>
-
-                    <button class="btn btn-primary btn-block btn-lg shadow-lg mt-3">{{ __('Log in') }}</button>
-                </form>
-
-                <div class="text-center mt-4 text-lg fs-4">
-                    <p class="text-gray-600">{{ __("Don't have an account") }}?
-                        <a href="/register" class="font-bold">
-                            {{ __('Sign up.') }}
-                        </a>
-                    </p>
-
-                    @if (Route::has('password.request'))
-                        <p>
-                            <a class="font-bold" href="{{ route('password.request') }}">
-                                {{ __('Forgot password') }}?
-                            </a>
-                        </p>
-                    @endif
+                    </form>
                 </div>
             </div>
         </div>
-
-        <div class="col-lg-6 d-none d-lg-block">
-            <div id="auth-right">
-            </div>
-        </div>
+        <a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top"
+            data-toggle="scroll-to-top"><i class="fa fa-angle-up"></i></a>
     </div>
-@endsection
+    <script src={{ asset('frontend/auth/js/vendor.min.js') }} type=1dcc4dafd62983dc732657cb-text/javascript></script>
+    <script src={{ asset('frontend/auth/js/app.min.js') }} type=1dcc4dafd62983dc732657cb-text/javascript></script>
+    <script src={{ asset('frontend/auth/js/demo/login-v2.demo.js') }} type=1dcc4dafd62983dc732657cb-text/javascript>
+    </script>
+    <script>
+        function myFunction() {
+            var x = document.getElementById("password");
+            if (x.type === "password") {
+                x.type = "text";
+            } else {
+                x.type = "password";
+            }
+        }
+    </script>
+</body>
+</html>
